6651dfbec47cdef6973c76d9dc021afb72efb807
sakaitakuma <63274132+sakaitakuma@users.noreply.github.com>
2020-05-23 18:35:38 +0900

    create view

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,18 +1,17 @@
 $(function(){
-  // $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
   const buildFileField = (num)=> {
     const html = `<div data-index="${num}" class="js-file_box">
     <input class="js-file" type="file"
-    name="product[images_attributes][${num}][src]"
-    id="product_images_attributes_${num}_src"><br>
-    <div class="js-remove">削除</div>
+    name="product[images_attributes][${num}][picture]"
+    id="product_images_attributes_${num}_picture"><br>
     </div>`;
     return html;
   }
   // プレビュー用のimgタグを生成する関数
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    const html = `<div><img class="picture" data-index="${index}" src="${url}" width="122px" height="146px"></div>`;
+    // <br><div class="js-btn"><div class="js-edit">編集</div><div class="js-remove">削除</div>
     return html;
   }
   
@@ -22,6 +21,7 @@ $(function(){
   lastIndex = $('.js-file_box:last').data('index');
   fileIndex.splice(0, lastIndex);
   
+  
   $('.hidden-destroy').hide();
   
   $('#image-box').on('change', '.js-file', function(e) {
@@ -29,31 +29,35 @@ $(function(){
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
-    
+    if( $('img.picture').length == 4) {
+      $('.image').css({"display": "none"})
+    }
+
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-      img.setAttribute('src', blobUrl);
+      img.setAttribute('picture', blobUrl);
     } else {  // 新規画像追加の処理
       $('#previews').append(buildImg(targetIndex, blobUrl));
+      let limitFileField = $(".js-file_box:last").data("index");
       // fileIndexの先頭の数字を使ってinputを作る
-      $('#image-box').append(buildFileField(fileIndex[0]));
+      $('#image-box').prepend(buildFileField(fileIndex[0]));
       fileIndex.shift();
       // 末尾の数に1足した数を追加する
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
     }
   });
   
-  $('#image-box').on('click', '.js-remove', function() {
-    const targetIndex = $(this).parent().data('index');
-    // 該当indexを振られているチェックボックスを取得する
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    // もしチェックボックスが存在すればチェックを入れる
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
+  // $('#previews').on('click', '.js-remove', function() {
+  //   const targetIndex = $(this).parent().data('index');
+  //   // 該当indexを振られているチェックボックスを取得する
+  //   const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+  //   // もしチェックボックスが存在すればチェックを入れる
+  //   if (hiddenCheck) hiddenCheck.prop('checked', true);
     
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
+  //   $(this).parent().remove();
+  //   $(`img[data-index="${targetIndex}"]`).remove();
     
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  });
+  //   // 画像入力欄が0個にならないようにしておく
+  //   if ($('.js-file').length == 0) $('#previews').append(buildFileField(fileIndex[0]));
+  // });
 });
