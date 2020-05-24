a17b0fa588632c1ef832e3cc59ec1e166294086d
yoshi-oz <hagimenoippo3150@gmail.com>
2020-05-24 11:58:18 +0900

    fixed-purchase-function

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,6 +1,6 @@
 class CardController < ApplicationController
   require "payjp"
-
+  before_action :set_card, only: [:delete, :show]
   def new
     card = Card.where(user_id: current_user.id)
     redirect_to action: "show" if card.exists?
@@ -28,26 +28,27 @@ class CardController < ApplicationController
   end
 
   def delete #PayjpとCardデータベースを削除します
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-    else
+    if @card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
-      card.delete
+      @card.delete
     end
-      redirect_to action: "new"
+    redirect_to action: "new"
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
     @parents = Category.where(ancestry: nil)
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
+    if @card.blank?
       redirect_to action: "new" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
+  end
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first
   end
 end
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,28 +1,26 @@
 class PurchasesController < ApplicationController
   require 'payjp'
   before_action :set_product
-
+  before_action :set_card, only: [:index, :pay]
   def index
-    card = Card.where(user_id: current_user.id).first
     #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
-    if card.blank?
+    if @card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       #保管した顧客IDでpayjpから情報取得
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
   def pay
-    card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     amount: @product.price, 
-    customer: card.customer_id, 
+    customer: @card.customer_id, 
     currency: 'jpy', 
   )
   redirect_to action: 'done' 
@@ -35,4 +33,9 @@ class PurchasesController < ApplicationController
   def set_product
     @product = Product.find(params[:product_id])
   end
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first
+  end
+
 end
